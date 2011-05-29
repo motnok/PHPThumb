d499cd0605d88307d09041349a8b19dcf6918f0f
Kristoffer Hansen <kriz@motnok.dk>
2011-05-30 00:19:24 +0200

    Added geometry interfaces

---
+++ PhpThumb/Geometry/RotatableInterface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Namespaced PHP 5.3 refactoring of PhpThumb 3.0 
+ * 
+ * PhpThumb 3.0 is originally by Ian Selby/Gen X Design:
+ * 	PhpThumb : PHP Thumb Library <http://phpthumb.gxdlabs.com>
+ * 	Copyright (c) 2009, Ian Selby/Gen X Design
+ * 	Author(s): Ian Selby <ian@gen-x-design.com>
+ *
+ * Copyright (c) 2011, Kristoffer Hansen
+ * Author(s): Kristoffer Hansen <kriz@motnok.dk>
+ *
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * @author Ian Selby <ian@gen-x-design.com>
+ * @author Kristoffer Hansen <kriz@motnok.dk>
+ * @copyright Copyright (c) 2009, Ian Selby/Gen X Design
+ * @copyright Copyright (c) 2011, Kristoffer Hansen
+ * @link http://phpthumb.gxdlabs.com
+ * @link https://github.com/motnok/PHPThumb
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @version 3.0n
+ * @package PhpThumb
+ * @filesource
+ */
+
+namespace PhpThumb\Geometry;
+
+/**
+ * RotatableInterface
+ * 
+ * Implements methods for basic rotating of image
+ * 
+ * @author Kristoffer Hansen <kriz@motnok.dk>
+ */
+interface RotatableInterface
+{
+	
+	/**
+	 * Rotate image clockwise by $degrees
+	 * 
+	 * @param int $degrees
+	 */
+	public function rotate($degrees);
+	
+}
